ce2dc29f50e2d311ded29225f3f87c06fcc8a9f6
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-28 20:42:01 +0200

    init

---
+++ src/test/java/java/net/digital_alexandria/tests/EnumTest.java
@@ -0,0 +1,48 @@
+/**
+ * sgl4j: a Java implementation of various latent variable models.
+ *
+ * Copyright (C) 2015 - 2016 Simon Dirmeier
+ *
+ * This file is part of sgl4j.
+ * <p>
+ * sgl4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * sgl4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with sgl4j.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package java.net.digital_alexandria.tests;
+
+import net.digital_alexandria.lvm4j.enums.ExitCode;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author Simon Dirmeier {@literal simon.dirmeier@gmx.de}
+ */
+public class EnumTest
+{
+
+    @Test
+    public void testExitCodeError() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
+        ExitCode e = ExitCode.EXIT_ERROR;
+        assert e.code() == -1;
+    }
+
+    @Test
+    public void testExitCodeSuccess() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
+        ExitCode e = ExitCode.EXIT_SUCCESS;
+        assert e.code() == 0;
+    }
+}
---
+++ src/test/java/java/net/digital_alexandria/tests/TestSuite.java
@@ -0,0 +1,46 @@
+/**
+ * sgl4j: a Java implementation of various latent variable models.
+ *
+ * Copyright (C) 2015 - 2016 Simon Dirmeier
+ *
+ * This file is part of sgl4j.
+ * <p>
+ * sgl4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * sgl4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with sgl4j.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package java.net.digital_alexandria.tests;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Simon Dirmeier {@literal simon.dirmeier@gmx.de}
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    SortTest.class, EnumTest.class, UtilTest.class
+})
+public class TestSuite
+{
+
+    @BeforeClass
+    public static void setup()
+    {
+        org.apache.log4j.ConsoleAppender appender = new org.apache.log4j.ConsoleAppender();
+        appender.setWriter(new java.io.OutputStreamWriter(java.lang.System.out));
+        appender.setLayout(new org.apache.log4j.PatternLayout("%-5p [%t]: %m%n"));
+        org.apache.log4j.Logger.getRootLogger().addAppender(appender);
+    }
+}
